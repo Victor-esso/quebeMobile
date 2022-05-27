4df1611f00c49ce4da2833b614105a8848362ae4
Victor Esso <victoresso97@gmail.com>
2022-05-27 16:47:01 +0100

    Stuff

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    
+
+    const starterSwiper = new Swiper('.starterSwiper',{
+        effect:"fade",
+        navigation: {
+            nextEl: '.starterSwiper-next',
+            prevEl: '.swiper-button-prev',
+          },
+        pagination: {
+            el: '.starterSwiper-pagnation',
+            clickable: true,
+        },
+    }); 
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
