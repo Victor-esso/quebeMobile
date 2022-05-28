0208c9f64bd3906af7fba0e6132008791b1d0f58
Victor Esso <victoresso97@gmail.com>
2022-05-28 02:24:32 +0100

    get_started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,4 @@
 $(document).ready(function(){
-    
-
-    const starterSwiper = new Swiper('.starterSwiper',{
-        effect:"fade",
-        navigation: {
-            nextEl: '.starterSwiper-next',
-            prevEl: '.swiper-button-prev',
-          },
-        pagination: {
-            el: '.starterSwiper-pagnation',
-            clickable: true,
-        },
-    }); 
-
-
 
     //Full ScreenMode
     $("#fullscreen-toggle").on('click', function() {
@@ -31,6 +16,48 @@ $(document).ready(function(){
             $("#fullscreen-toggle").removeClass('active');
         }
     });
+
+
+
+    // Stater swiper js
+    const starterSwiper = new Swiper('.starterSwiper',{
+        effect:"fade",
+        navigation: {
+            nextEl: '.starterSwiper-next',
+            prevEl: '.swiper-button-prev',
+          },
+        pagination: {
+            el: '.starterSwiper-pagnation',
+            clickable: true,
+        },
+    });
+
+
+    // Handles getting to the last slide and moving to another page
+    starterSwiper.on('slideChange',function(){
+        var starterSwiperNext = $('.starterSwiper-next');
+        const reachEnd = () => {
+            if(starterSwiper.isEnd && (starterSwiper.activeIndex == starterSwiper.slides.length-1) ){
+                window.location.href = starterSwiperNext.attr('href');
+            }
+        }
+
+            if(starterSwiper.isEnd && (starterSwiper.activeIndex == starterSwiper.slides.length-1) ){
+                starterSwiperNext.removeAttr('disabled');
+                starterSwiperNext.on('click',reachEnd);
+                starterSwiper.on('touchEnd',reachEnd);
+            }else{
+                starterSwiperNext.off('click');
+                starterSwiper.off('touchEnd',reachEnd);
+            }
+ 
+            
+             
+    })
+
+    
+
+    
 
 
 
